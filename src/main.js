cfa19d249806792783e2eb25746821ab422637ca
OlhaMasl <maslakok@gmail.com>
2024-06-01 16:51:00 +0300

    HW11-part02

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import "./js/pixabay-api";
+import "./js/render-functions";
+
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const formEL = document.querySelector(".form");
+const galleryList = document.querySelector(".gallary");
+
+formEL.addEventListener("submit", evt => {
+    evt.preventDefault();
+    const InputValue = evt.target.elements.search.value.trim();
+
+    searchImage(InputValue).then(data => {
+    console.log(data.hits)
+    })
+    
+    galleryList.innerHTML = markup;
+console.log(galleryList);
+
+const galleryModel = new SimpleLightbox('.gallery a', { 
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+galleryModel.on('show.simplelightbox', function () {
+	// do something…
+});
+
+});
+

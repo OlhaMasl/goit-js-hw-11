8a906604d86091cc3a260a2b8d974caf1097085b
OlhaMasl <maslakok@gmail.com>
2024-06-05 22:42:28 +0300

    HW11-version01

--- src/main.js
+++ src/main.js
@@ -6,15 +6,34 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+
 const formEL = document.querySelector(".form");
 const galleryList = document.querySelector(".gallary");
+const loaderEl = document.querySelector(".loader");
 
 formEL.addEventListener("submit", evt => {
     evt.preventDefault();
     const InputValue = evt.target.elements.search.value.trim();
-
+    if (InputValue === "") {
+      return  
+    };
+    evt.target.reset();
+    loaderEl.classList.remove("is-hiding");
     searchImage(InputValue).then(data => {
-        console.log(data.hits);
+        // console.log(data);
+        if (data.total === 0) {
+         iziToast.show({
+              message: 'Sorry, there are no images matching your search query. Please try again!',
+              messageColor: 'white',
+              backgroundColor: 'red',
+             position: 'topRight',
+          });
+        }
         const markup = imagesTemplate(data.hits);
         galleryList.innerHTML = markup;
         const galleryModel = new SimpleLightbox('.gallary a', {
@@ -23,10 +42,21 @@ formEL.addEventListener("submit", evt => {
             captionDelay: 250,
         });
         galleryModel.on('show.simplelightbox', function () {
-            // do something…
+            refresh();
         }); 
-    })
-    
+    }).catch(error => {
+        // console.log(error);
+        iziToast.show({
+            message: 'Internal server error!',
+            messageColor: 'white',
+            backgroundColor: 'red',
+            position: 'topRight',
+        })
+          
+    }).finally(() => {
+        loaderEl.classList.add("is-hiding");
+            });
+   
 });
 
      
